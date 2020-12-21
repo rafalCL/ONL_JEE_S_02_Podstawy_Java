d6c5ce8f8ea41214ae8b92900747fed1eaa9ef96
Robot CodersLab <26678259+CodersLabBot@users.noreply.github.com>
2020-12-21 11:29:01 +0100

    Initial commit

---
+++ src/main/java/pl/coderslab/externaljar/Main02.java
@@ -0,0 +1,8 @@
+package pl.coderslab.externaljar;
+
+public class Main02 {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/pl/coderslab/externaljar/Main04.java
@@ -0,0 +1,8 @@
+package pl.coderslab.externaljar;
+
+public class Main04 {
+
+    public static void main(String[] args) {
+
+    }
+}
