9136e3b2d8f5164cbbffee5f506c8d1c0ac3dffa
Rafal <rafal.opial@coderslab.pl>
2021-01-18 15:46:19 +0100

    m1d1t2z1 tworzenie metod

--- src/main/java/pl/coderslab/methods/Main01.java
+++ src/main/java/pl/coderslab/methods/Main01.java
@@ -4,4 +4,10 @@ public class Main01 {
 
     public static void main(String[] args) {
     }
+
+    public static int multiply(int multipler, int index) {
+        int result = multipler * index;
+
+        return result;
+    }
 }
