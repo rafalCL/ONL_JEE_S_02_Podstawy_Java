15a96ebb643ac18c6e19c135813f69612c2dc68d
Rafal <rafal.opial@coderslab.pl>
2021-01-18 15:48:36 +0100

    m1d1t2z1 wywolanie metody

--- src/main/java/pl/coderslab/methods/Main01.java
+++ src/main/java/pl/coderslab/methods/Main01.java
@@ -3,6 +3,9 @@ package pl.coderslab.methods;
 public class Main01 {
 
     public static void main(String[] args) {
+        int multiplied = multiply(2,3);
+
+        System.out.println(multiplied);
     }
 
     public static int multiply(int multipler, int index) {
