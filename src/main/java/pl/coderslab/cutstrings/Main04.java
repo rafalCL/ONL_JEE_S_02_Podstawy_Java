0c69c9453225bdbaf12d8baaca165c32a044847b
Rafal <rafal.opial@coderslab.pl>
2021-01-19 16:34:52 +0100

    m1d2t3z4 solution to excercise 4 and split with two parameters example

--- src/main/java/pl/coderslab/cutstrings/Main04.java
+++ src/main/java/pl/coderslab/cutstrings/Main04.java
@@ -1,9 +1,14 @@
 package pl.coderslab.cutstrings;
 
+import java.util.Arrays;
+
 public class Main04 {
 
     public static void main(String[] args) {
+        System.out.println(Arrays.toString("a-b-c-d".split("-", 3)));
+    }
 
-
+    public static String[] onlyTwoElements(String str, char separator){
+        return str.split(String.valueOf(separator), 2);
     }
 }
