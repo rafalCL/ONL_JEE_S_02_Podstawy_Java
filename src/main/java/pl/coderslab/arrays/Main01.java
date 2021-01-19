5d9837a552197c7ddd08d4793187d923e8e19c85
Rafal <rafal.opial@coderslab.pl>
2021-01-19 09:53:13 +0100

    m1d1t4z1 tablice

--- src/main/java/pl/coderslab/arrays/Main01.java
+++ src/main/java/pl/coderslab/arrays/Main01.java
@@ -3,7 +3,29 @@ package pl.coderslab.arrays;
 public class Main01 {
 
     public static void main(String[] args) {
+//        int[] arrIntegers = {1,2,3,4};
+//        int element0 = arrIntegers[0];
+//        int element2 = arrIntegers[2];
+//        int lastElem = arrIntegers[arrIntegers.length - 1];
 
+        int[] mainTab = new int[50];
 
+        for(int i = 0; i< mainTab.length; i++){
+            mainTab[i] = i;
+        }
+
+        for(int i = 0; i< mainTab.length; i++){
+            if(i % 10 == 0 && i != 0){
+                System.out.println();
+            }
+            if(mainTab[i] < 10){
+                System.out.print("0");
+            }
+            System.out.print(mainTab[i]);
+
+            if(i != mainTab.length-1) {
+                System.out.print(", ");
+            }
+        }
     }
 }
