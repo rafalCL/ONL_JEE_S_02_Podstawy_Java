f07d13e90299852a908cb52dd145ea79d1769990
Rafal <rafal.opial@coderslab.pl>
2021-01-19 15:42:23 +0100

    m1d2t3z1 cut string and examples for: String.indexOf and StringBuilder

--- src/main/java/pl/coderslab/cutstrings/Main01.java
+++ src/main/java/pl/coderslab/cutstrings/Main01.java
@@ -1,10 +1,18 @@
 package pl.coderslab.cutstrings;
 
+//W pliku `Main01.java` napisz program, który:
+//
+//        1. podzieli na wyrazy ciąg znaków przechowywany w zmiennej `str`, znajdującej się w pliku,
+//        2. wyświetli na konsoli każdy z tych wyrazów w oddzielnej linii.
+
 public class Main01 {
-
     public static void main(String[] args) {
-
-    String str = "Naucz się programować od podstaw i rozwiń wymarzoną karierę w branży IT.";
-
+        String str = "Naucz się programować od podstaw i rozwiń wymarzoną karierę w branży IT.";
+//        1. podzieli na wyrazy ciąg znaków przechowywany w zmiennej `str`, znajdującej się w pliku,
+        String[] words = str.split(" ");
+//        2. wyświetli na konsoli każdy z tych wyrazów w oddzielnej linii.
+        for (String word : words){
+            System.out.println(word);
+        }
     }
 }
--- src/main/java/pl/coderslab/strings/Main02.java
+++ src/main/java/pl/coderslab/strings/Main02.java
@@ -3,8 +3,19 @@ package pl.coderslab.strings;
 public class Main02 {
 
     public static void main(String[] args) {
+        int position = "abcd".indexOf('c');
 
+        StringBuilder sb = new StringBuilder();
 
+        sb  // []
+            .append("t1") // ['t','1']
+            .append("t2 ") // ['t','1', 't', '2', ' ']
+            .append(10) // ['t','1', 't', '2', ' ', '1', '0']
+            .append(' '); // ['t','1', 't', '2', ' ', '1', '0', ' ']
+
+        sb.reverse();
+        String built = sb.toString();
+        System.out.println(built);
     }
 
 }
