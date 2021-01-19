5c7eb8d47882864557d4390d0c4cb8694441ce99
Rafal <rafal.opial@coderslab.pl>
2021-01-19 13:04:10 +0100

    m1d2t2z1 String class

--- src/main/java/pl/coderslab/strings/Main01.java
+++ src/main/java/pl/coderslab/strings/Main01.java
@@ -1,10 +1,27 @@
 package pl.coderslab.strings;
 
+//## Zadanie 1 - rozwiązywane z wykładowcą
+//
+//        W pliku `Main01.java` napisz program, który:
+//
+//        1. pobierze przedostatnią literę zmiennej `str`, znajdującej się w pliku,
+//        2. wyświetli na konsoli liczbę wystąpień tej litery w całym ciągu znaków.
+
 public class Main01 {
 
     public static void main(String[] args) {
+        String str = "CodersLab jak dobrze zacząć programować";
 
-    String str = "CodersLab jak dobrze zacząć programować";
+        char secondLastLetter = str.charAt(str.length()-2);
+        System.out.println(secondLastLetter);
 
+        int count = 0;
+
+        for(int i = 0; i<str.length(); i++){
+            if(str.charAt(i)==secondLastLetter){
+                count++;
+            }
+        }
+        System.out.println(count);
     }
 }
---
+++ src/main/java/pl/coderslab/strings/StaticNonStaticExample.java
@@ -0,0 +1,10 @@
+package pl.coderslab.strings;
+
+public class StaticNonStaticExample {
+    public static void main(String[] args) {
+        String str1 = " ala ma kota ";
+        int len = str1.length();
+        String.join("");
+
+    }
+}
