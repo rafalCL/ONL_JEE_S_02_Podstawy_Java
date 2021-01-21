5e8bed24c4bb6e38ddeccbb807dd3e2d097cabfb
Rafal <rafal.opial@coderslab.pl>
2021-01-21 14:54:48 +0100

    extended create file example

--- src/main/java/pl/coderslab/filesnio/Main02.java
+++ src/main/java/pl/coderslab/filesnio/Main02.java
@@ -1,9 +1,24 @@
 package pl.coderslab.filesnio;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main02 {
 
     public static void main(String[] args) {
+        try {
+            Files.createDirectory(Paths.get("ala"));
+            extendedCreateFile("ala", "makota.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
+    }
 
+    static void extendedCreateFile(String directoryName, String fileName) throws IOException {
+        Path p = Paths.get(directoryName, fileName);
+        Files.createFile(p);
     }
 }
